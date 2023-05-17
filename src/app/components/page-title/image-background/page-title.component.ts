9a73deb2dd7fc4b65c8eb7f6ef9b68dfe42a138c
RDoneux <robertdoneux@gmail.com>
2023-05-17 17:38:35 +0100

    loads variable image depending on display type

--- src/app/components/page-title/image-background/page-title.component.ts
+++ src/app/components/page-title/image-background/page-title.component.ts
@@ -1,12 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-page-title',
   templateUrl: './page-title.component.html',
   styleUrls: ['./page-title.component.scss'],
 })
-export class PageTitleComponent {
+export class PageTitleComponent implements OnInit {
   @Input({ required: true }) titleText!: string;
   @Input({ required: true }) imageSrc!: string;
   @Input() type: 'landing' | 'default' = 'default';
+
+  imageSourceDesktop: string = '';
+  imageSourceMobile: string = '';
+
+  ngOnInit(): void {
+    const parts: string[] = this.imageSrc.split('.');
+    this.imageSourceDesktop = `${parts[0]}__desktop.${parts[1]}`;
+    this.imageSourceMobile = `${parts[0]}__mobile.${parts[1]}`;
+  }
 }
